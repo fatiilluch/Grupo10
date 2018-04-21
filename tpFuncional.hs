74601f3a29fd6341e58e72c3ef3970e1573236b5
sashaestrella <37596403+sashaestrella@users.noreply.github.com>
2018-04-21 16:38:41 -0300

    Update tpFuncional.hs
    Edité los test para que queden con mejor expresividad.

--- tpFuncional.hs
+++ tpFuncional.hs
@@ -7,28 +7,28 @@ import Test.Hspec
  ---------------------------------------------------------------- Testing --------------------------------------------------------------- 
 
 ejecutarTests = hspec $ do
-   describe "Eventos (Con billetera de 10)" $ do
-    it "Depositar 10 más. Deberia quedar con 20 monedas" $ depósito 10 10 `shouldBe` 20
-    it "Extraer 3: Deberia quedar con 7" $ extracción 3 10 `shouldBe` 7
-    it "Extraer 15: Deberia quedar con 0" $ extracción 15 10 `shouldBe` 0
-    it "Un upgrade: Deberia quedar con 12" $ upgrade 10 `shouldBe` 12
-    it "Cerrar la cuenta : 0" $ cerrarCuenta 10 `shouldBe` 0
-    it "Queda igual: 10" $ quedaIgual 10 `shouldBe` 10
-    it "Depositar 1000, y luego tener un upgrade: 1020" $ (upgrade.depósito 1000) 10 `shouldBe` 1020
+   describe "Eventos (billetera inicial con 10 monedas)" $ do
+    it "Luego de depositar 10 monedas más, la billetera deberia quedar con 20 monedas" $ depósito 10 10 `shouldBe` 20
+    it "Luego de extraer 3 monedas : La billetera deberia quedar con 7" $ extracción 3 10 `shouldBe` 7
+    it "Luego de extraer 15 monedas : La billetera deberia quedar vacía" $ extracción 15 10 `shouldBe` 0
+    it "Con un upgrade: La billetera deberia quedar con 12 monedas" $ upgrade 10 `shouldBe` 12
+    it "Al cerrar la cuenta : queda en 0" $ cerrarCuenta 10 `shouldBe` 0
+    it "La billetera queda igual, como al incio, con 10 monedas" $ quedaIgual 10 `shouldBe` 10
+    it "Luego de depositar 1000, y luego de tener un upgrade: la billetera queda con 1020 monedas" $ (upgrade.depósito 1000) 10 `shouldBe` 1020
    describe "Usuarios" $ do
-    it "Billetera de pepe es 10" $ billetera pepe `shouldBe` 10
-    it "Billetera de pepe luego de un cierre de cuenta : 0" $ cerrarCuenta (billetera pepe) `shouldBe` 0
-    it "Billetera de pepe luego de Depositar 15, extraer 2 y tener un upgrade : 27.6" $ (upgrade . (extracción 2) . (depósito 15)) (billetera pepe) `shouldBe` 27.6
+    it "La billetera de pepe tiene 10 monedas" $ billetera pepe `shouldBe` 10
+    it "La billetera de pepe luego de un cierre de cuenta queda vacía" $ cerrarCuenta (billetera pepe) `shouldBe` 0
+    it "La billetera de pepe luego de depositar 15 monedas, extraer 2 y tener un upgrade queda con 27.6 monedas" $ (upgrade . (extracción 2) . (depósito 15)) (billetera pepe) `shouldBe` 27.6
    describe "Transacciones" $ do
-    it "Aplicar transaccion 1 a pepe: quedaIgual 20 : 20" $ (aplicarTransacciones uno pepe) 20 `shouldBe` 20
-    it "Aplicar transaccion 2 a pepe: (Depositar 5) 10 : 15 " $ (aplicarTransacciones dos pepe) 10 `shouldBe` 15
-    it "Aplicar transaccion 2 a pepeDos: (Depositar 5) 50 : 55" $ (aplicarTransacciones dos pepeDos) 50 `shouldBe` 55
+    it "Aplicar transaccion 1 a pepe: quedaIgual 20 : 20" $ (aplicarTransaccion uno pepe) 20 `shouldBe` 20
+    it "Aplicar transaccion 2 a pepe: (Depositar 5) 10 : 15 " $ (aplicarTransaccion dos pepe) 10 `shouldBe` 15
+    it "Aplicar transaccion 2 a pepeDos: (Depositar 5) 50 : 55" $ (aplicarTransaccion dos pepeDos) 50 `shouldBe` 55
    describe "Nuevos Eventos" $ do
-    it "Aplicar transacción tres lucho: (tocoYMeVoy) 10 : 0" $ (aplicarTransacciones tres lucho) 10 `shouldBe` 0
-    it "Aplicar transacción cuatro lucho: (ahorranteErrante) 10 : 34" $ (aplicarTransacciones cuatro lucho) 10 `shouldBe` 34
+    it "Aplicar transacción tres a lucho: (tocoYMeVoy) 10 : 0" $ (aplicarTransaccion tres lucho) 10 `shouldBe` 0
+    it "Aplicar transacción cuatro a lucho: (ahorranteErrante) 10 : 34" $ (aplicarTransaccion cuatro lucho) 10 `shouldBe` 34
    describe "Pagos entre Usuarios" $ do
-    it "Aplicar transacción cinco pepe: (extracción 7) 10 : 3" $ (aplicarPagos cinco pepe) 10 `shouldBe` 3
-    it "Aplicar transacción cinco lucho: (depósito 7) 10 : 17" $ (aplicarPagos cinco lucho) 10 `shouldBe` 17
+    it "Aplicar transacción cinco a pepe: (extracción 7) 10 : 3" $ (aplicarPagos cinco pepe) 10 `shouldBe` 3
+    it "Aplicar transacción cinco a lucho: (depósito 7) 10 : 17" $ (aplicarPagos cinco lucho) 10 `shouldBe` 17
 
 pepeDos = modificarBilletera pepe 20
 
@@ -80,8 +80,8 @@ data Transacciones = Transacciones{
     evento :: Evento                
 } deriving (Show)
 
-aplicarTransacciones :: Transacciones -> Usuario -> Evento
-aplicarTransacciones transacción otroUsuario
+aplicarTransaccion :: Transacciones -> Usuario -> Evento
+aplicarTransaccion transacción otroUsuario
                 | nombre (usuario transacción) == nombre (otroUsuario) = evento transacción
                 | otherwise = quedaIgual
 
