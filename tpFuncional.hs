5deec477b0d8784638c52baae7850b26c9a30837
sashaestrella <37596403+sashaestrella@users.noreply.github.com>
2018-05-14 23:14:17 -0300

    Transacciones

--- tpFuncional.hs
+++ tpFuncional.hs
@@ -17,6 +17,25 @@ import Test.Hspec
 ---------------------------------------------------------------- Transacciones --------------------------------------------------------------- 
 
 
+type Evento = Float
+type Transacción = Usuario -> Evento
+
+generarTransacciones ::  Usuario -> Billetera -> Transacción 
+generarTransacciones usuario1 evento usuario2 
+  | funcionQueComparaNombres usuario1 usuario2  =  (evento.billetera) usuario2
+  | otherwise =  (quedaIgual.billetera) usuario2
+
+funcionQueComparaNombres :: Usuario -> Usuario -> Bool
+funcionQueComparaNombres usuario1 usuario2 = ( == ) (nombre usuario1) (nombre usuario2)
+
+nuevaBilleteraPara :: Usuario -> Dinero -> Usuario
+nuevaBilleteraPara usuario nuevoMonedero = usuario { billetera = nuevoMonedero}
+
+transacción1 :: Transacción
+transacción1 = generarTransacciones lucho cerrarCuenta
+
+transacción2 :: Transacción
+transacción2 = generarTransacciones pepe (depósito 5)
 
 
 
