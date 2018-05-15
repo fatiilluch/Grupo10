785e00ebec28dde75e34618668df0dfb60cdb096
Fátima Lluch <32984924+fatiilluch@users.noreply.github.com>
2018-05-14 23:40:27 -0300

    Carga de Tests Nuevos Eventos
    Se incluyeron los items 14 y 15 del TP Parte 1

--- tpFuncional.hs
+++ tpFuncional.hs
@@ -5,8 +5,8 @@ import Data.Maybe
 import Test.Hspec 
 
  ---------------------------------------------------------------- Testing --------------------------------------------------------------- 
-   ejecutarTests = hspec $do
- describe "Eventos: " $do 
+ ejecutarTests = hspec $ do
+ describe "Eventos: " $ do 
   it " Aplicar Depositar 10 más Debería quedar con 20 monedas "  deposito 10 'shouldBe' 20
   it " Extraer 3 y Debería quedar con 7 " extracción 3 'shouldBe'7
   it " Extraer 15 y Debería quedar con 0 " extracción 15 'shouldBe'0 
@@ -14,13 +14,13 @@ import Test.Hspec
   it " Cerrar la cuenta quedaria 0 " cerrarCuenta 'shouldBe' 0.0 
   it " Queda igual 10 " quedaIgual 'shouldBe' billetera 
   it " Depositar 1000 y luego tener un upgrade 1020 " depósito 1000 ( upgrade 'shouldBe' 1020)
-   
-   
-describe "Transacciones: " $do 
+describe "Transacciones: " $ do 
   it "El resultado de aplicar la transacción 1 a pepe con una billetera de 20 monedas debería ser 20, porque queda igual" $ transacción1 (nuevaBilleteraPara pepe 20) `shouldBe` 20 
   it "El resultado de aplicar la transacción 2 a pepe con una billetera de 10 monedas debería ser 15, porque deposita 5 monedas" $ transacción2 pepe `shouldBe` 15 
   it "El resultado de aplicar la transacción 2 a pepeDos con una billetera de 50 monedas debería ser 55, porque deposita 5 monedas" $ transacción2 (nuevaBilleteraPara pepeDos 50) `shouldBe` 55  
-
+describe "Nuevos Eventos: " $ do
+  it "El resultado de aplicar la transaccion 3 a lucho con una billetera de 10 monedas debería ser 0, ya que cierra la cuenta" $ transacción3 (nuevaBilleteraPara lucho 10) `shouldBe` 0 
+  it "El resultado de aplicar la transaccion 4 a lucho con una billetera de 10 monedas debería ser 34" $ transacción4 (nuevaBilleteraPara lucho 10) `shouldBe` 34 
 
  ---------------------------------------------------------------- Eventos --------------------------------------------------------------- 
 
@@ -54,8 +54,6 @@ lucho = Usuario "Luciano" 2.0
 pepeDos = Usuario "Jose" 20.0
 
 ---------------------------------------------------------------- Transacciones --------------------------------------------------------------- 
-
-
 type Evento = Float
 type Transacción = Usuario -> Evento
 
@@ -76,9 +74,8 @@ transacción1 = generarTransacciones lucho cerrarCuenta
 transacción2 :: Transacción
 transacción2 = generarTransacciones pepe (depósito 5)
 
-
-
-
-
- 
  ---------------------------------------------------------------- Nuevos Eventos --------------------------------------------------------------- 
+
+
+
+
