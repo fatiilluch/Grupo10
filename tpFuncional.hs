3013f26d196310c8dfd18e3a9db40b764ae308a5
sashaestrella <37596403+sashaestrella@users.noreply.github.com>
2018-05-14 23:16:46 -0300

    Usuarios

--- tpFuncional.hs
+++ tpFuncional.hs
@@ -5,6 +5,10 @@ import Data.Maybe
 import Test.Hspec 
 
  ---------------------------------------------------------------- Testing --------------------------------------------------------------- 
+    describe "Transacciones: " $do 
+        it "El resultado de aplicar la transacción 1 a pepe con una billetera de 20 monedas debería ser 20, porque queda igual" $ transacción1 (nuevaBilleteraPara pepe 20) `shouldBe` 20 
+        it "El resultado de aplicar la transacción 2 a pepe con una billetera de 10 monedas debería ser 15, porque deposita 5 monedas" $ transacción2 pepe `shouldBe` 15 
+        it "El resultado de aplicar la transacción 2 a pepeDos con una billetera de 50 monedas debería ser 55, porque deposita 5 monedas" $ transacción2 (nuevaBilleteraPara pepeDos 50) `shouldBe` 55  
 
 
  ---------------------------------------------------------------- Eventos --------------------------------------------------------------- 
@@ -13,6 +17,14 @@ import Test.Hspec
 
  ---------------------------------------------------------------- Usuarios --------------------------------------------------------------- 
 
+data Usuario = Usuario {
+    nombre :: String,
+    billetera :: Dinero
+ } deriving (Show, Eq)
+
+pepe  = Usuario "Jose" 10.0  
+lucho = Usuario "Luciano" 2.0
+pepeDos = Usuario "Jose" 20.0
 
 ---------------------------------------------------------------- Transacciones --------------------------------------------------------------- 
 
